759486e6004df571b85d2f9c8a9141bbe95e4479
itsu020402 <itsu020402@gmail.com>
2017-08-15 23:29:52 +0900

    !?

--- src/main/java/net/comorevi/moneyapi/EventListener.java
+++ src/main/java/net/comorevi/moneyapi/EventListener.java
@@ -1,24 +1,24 @@
 package net.comorevi.moneyapi;
 
-import cn.nukkit.event.Listener;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 
 public class EventListener implements Listener{
-	
-	private MoneySAPI plugin;
-	private int defaultmoney = 500;
 
-	public EventListener(MoneySAPI plugin) {
-		this.plugin = plugin;
-	}
-	
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event){
-		plugin.getSQL().createAccount(event.getPlayer().getName(), this.defaultmoney);
-		if(!plugin.getSQL().isRegister(event.getPlayer().getName())){
-			plugin.getServer().broadcastMessage("[MoneySAPI]次のプレイヤーのアカウントを作成しました。 \n"+" - "+event.getPlayer().getName());
-		}
-	}
-	
+    private MoneySAPI plugin;
+    private int defaultmoney = 5000;
+
+    public EventListener(MoneySAPI plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        plugin.getSQL().createAccount(event.getPlayer().getName(), this.defaultmoney);
+        if(!plugin.getSQL().isRegister(event.getPlayer().getName())){
+            plugin.getServer().broadcastMessage("[MoneySAPI]次のプレイヤーのアカウントを作成しました。 \n"+" - "+event.getPlayer().getName());
+        }
+    }
+
 }
