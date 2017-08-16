dadef01912d98b83dcc86e10c7a689d9d0eef799
popkechupki <hobbypopkechupki@gmail.com>
2017-08-16 11:10:33 +0900

    fix

--- src/main/java/net/comorevi/moneyapi/EventListener.java
+++ src/main/java/net/comorevi/moneyapi/EventListener.java
@@ -15,7 +15,8 @@ public class EventListener implements Listener{
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        if(plugin.getSQL().createAccount(event.getPlayer().getName(), this.defaultmoney)){
+    	plugin.getSQL().createAccount(event.getPlayer().getName(), this.defaultmoney);
+        if(!plugin.getSQL().isRegister(event.getPlayer().getName())){
             plugin.getServer().broadcastMessage("[MoneySAPI]次のプレイヤーのアカウントを作成しました。 \n"+" - "+event.getPlayer().getName());
         }else{
         	event.getPlayer().sendMessage("[MoneySAPI]アカウントをロードしました。(現在の所持金: " + plugin.getMoney(event.getPlayer()) + MoneySAPI.unit + ")");
