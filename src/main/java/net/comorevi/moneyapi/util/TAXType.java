ca82554bb8fb40516fdb996a8f0bf80e06096ae3
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-03-20 16:05:35 +0900

    Update TAXType.java

--- src/main/java/net/comorevi/moneyapi/util/TAXType.java
+++ src/main/java/net/comorevi/moneyapi/util/TAXType.java
@@ -3,7 +3,7 @@ package net.comorevi.moneyapi.util;
 public class TAXType {
     public static final double PAY = 1.05;
     public static final double CHEST_SHOP = 1.05;
-    public static final double ADMIN_SHOP = 1.08;
+    public static final double ADMIN_SHOP = 1.10;
     public static final double INCOME_LOW = 1.05;
     public static final double INCOME_MEDIUM = 1.08;
     public static final double INCOME_HIGH = 1.10;
