8ecd7b6c3f8a1ae71b2adc4b3ce1f1f518b031fe
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-04-01 17:03:19 +0900

    Implement takemoney command.

--- src/main/java/net/comorevi/moneyapi/command/TakeMoneyCommand.java
+++ src/main/java/net/comorevi/moneyapi/command/TakeMoneyCommand.java
@@ -2,14 +2,50 @@ package net.comorevi.moneyapi.command;
 
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import net.comorevi.moneyapi.MoneySAPI;
+import net.comorevi.moneyapi.util.MessageManager;
+import net.comorevi.moneyapi.util.TextValues;
 
 public class TakeMoneyCommand extends Command {
     public TakeMoneyCommand(String name) {
-        super(name);
+        super(name, "プレイヤーから指定した金額を没収します。", "/takemoney <target: 名前> <amount: 金額>");
     }
 
     @Override
     public boolean execute(CommandSender commandSender, String s, String[] strings) {
-        return false;
+        if (commandSender.hasPermission("moneysapi.command.takemoney")) {
+            commandSender.sendMessage(TextValues.ALERT+ MessageManager.getInstance().translateString("error-no-permission"));
+            return true;
+        }
+
+        if (strings.length < 2) {
+            commandSender.sendMessage(TextValues.ALERT+MessageManager.getInstance().translateString("error-command-args"));
+            return false;
+        }
+
+        if (!MoneySAPI.getInstance().existsAccount(strings[0])) {
+            commandSender.sendMessage(TextValues.ALERT+MessageManager.getInstance().translateString("player-account-not-found", strings[0]));
+            return false;
+        }
+
+        if (!isDigit(strings[1]) || Integer.parseInt(strings[1]) < 0) {
+            commandSender.sendMessage(TextValues.ALERT+MessageManager.getInstance().translateString("error-command-args-num"));
+            return false;
+        }
+
+        MoneySAPI.getInstance().reduceMoney(strings[0], Integer.parseInt(strings[1]));
+        commandSender.sendMessage(TextValues.INFO+MessageManager.getInstance().translateString("player-account-money-take", strings[0], strings[1], MoneySAPI.UNIT));
+        return true;
+    }
+
+    private boolean isDigit(String str) {
+        boolean isDigit = true;
+        for (int i = 0; i < str.length(); i++) {
+            isDigit = Character.isDigit(str.charAt(i));
+            if (!isDigit) {
+                break;
+            }
+        }
+        return isDigit;
     }
 }
