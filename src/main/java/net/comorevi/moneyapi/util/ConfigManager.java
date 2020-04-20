7f9c54df5148ff528b7a7046b631e4dad2112f38
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-04-20 16:17:24 +0900

    Add new properties.

--- src/main/java/net/comorevi/moneyapi/util/ConfigManager.java
+++ src/main/java/net/comorevi/moneyapi/util/ConfigManager.java
@@ -4,16 +4,18 @@ import cn.nukkit.utils.Config;
 import cn.nukkit.utils.ConfigSection;
 
 import java.io.File;
+import java.util.List;
 
 public class ConfigManager {
-    private static final ConfigManager instance = new ConfigManager();
+    private static ConfigManager instance = new ConfigManager();
     private Config config;
     public static int DEFAULT_MONEY;
     public static boolean DEFAULT_PUBLISH_STATUS;
     public static String MONEY_UNIT;
     public static boolean PAY_COMMISSION;
 
-    public ConfigManager() {
+    private ConfigManager() {
+        instance = this;
         new File("./plugins/MoneySAPI").mkdirs();
         if (!new File("./plugins/MoneySAPI/", "config.yml").exists()) {
             createConfig();
@@ -26,6 +28,41 @@ public class ConfigManager {
         PAY_COMMISSION = config.getBoolean("PayCommission", false);
     }
 
+    public static ConfigManager getInstance() {
+        return instance;
+    }
+
+    public int getExchangeRate() {
+        return config.getInt("ExchangeRate");
+    }
+
+    public int getRatePattern() {
+        return config.getInt("RatePattern");
+    }
+
+    public int getPatternPhase() {
+        return config.getInt("PatternPhase");
+    }
+
+    public void setExchangeRate(int rate) {
+        config.set("ExchangeRate", rate);
+        config.save();
+    }
+
+    public void setPatternPhase(int phase) {
+        config.set("PatternPhase", phase);
+        config.save();
+    }
+
+    public void setRatePattern(int pattern) {
+        config.set("RatePattern", pattern);
+        config.save();
+    }
+
+    public List<String> getIgnoreWorlds() {
+        return config.getStringList("IgnoreWorlds");
+    }
+
     private void createConfig() {
         ConfigSection cs = new ConfigSection(){
             {
@@ -33,6 +70,10 @@ public class ConfigManager {
                 put("Publish", false);
                 put("Unit", "MS");
                 put("PayCommission", false);
+                put("RatePattern", 1);
+                put("PatternPhase", 1);
+                put("ExchangeRate", 150);
+                put("IgnoreWorlds", List.of("central", "life2020-01"));
             }
         };
         config = new Config(new File("./plugins/MoneySAPI/", "config.yml"), Config.YAML, cs);
