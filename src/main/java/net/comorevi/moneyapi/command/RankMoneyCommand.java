859956506b040d4ef38a177f3567cb35b9e6adba
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-05-28 00:30:04 +0900

    Add money ranking.

--- src/main/java/net/comorevi/moneyapi/command/RankMoneyCommand.java
+++ src/main/java/net/comorevi/moneyapi/command/RankMoneyCommand.java
@@ -2,14 +2,28 @@ package net.comorevi.moneyapi.command;
 
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import net.comorevi.moneyapi.MoneySystemPlugin;
+import net.comorevi.moneyapi.util.MessageManager;
+import net.comorevi.moneyapi.util.TextValues;
 
 public class RankMoneyCommand extends Command {
     public RankMoneyCommand(String name) {
-        super(name);
+        super(name, "所持金ランキングを表示します。", "/rankmoney <順位: 何位までか>");
     }
 
     @Override
     public boolean execute(CommandSender commandSender, String s, String[] strings) {
-        return false;
+        if (!commandSender.hasPermission("moneysapi.command.moneyrank")) {
+            commandSender.sendMessage(TextValues.ALERT+ MessageManager.getInstance().translateString("error-no-permission"));
+            return true;
+        }
+        if (strings.length == 0) {
+            commandSender.sendMessage(TextValues.ALERT+MessageManager.getInstance().translateString("error-command-args"));
+            return false;
+        }
+
+        commandSender.sendMessage("=== "+MessageManager.getInstance().translateString("player-rank-title")+" ===");
+        commandSender.sendMessage(MoneySystemPlugin.getInstance().getRankString(Integer.parseInt(strings[0])));
+        return true;
     }
 }
